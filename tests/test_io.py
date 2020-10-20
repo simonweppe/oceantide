ba26f0214c686959bd7feb167819e25e97f44bb4
Rafael Guedes <r.guedes@oceanum.science>
2020-10-21 12:20:42 +1300

    Testing for otis binary format

---
+++ tests/test_io.py
@@ -0,0 +1,24 @@
+"""Test tide accessor."""
+import os
+import pytest
+import datetime
+import xarray as xr
+
+from oceantide import Tide
+from oceantide import read_otis_merged, read_otis_netcdf, read_otis_binary
+
+
+FILES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_files")
+
+
+def test_read_otis_merged():
+    ncfile = os.path.join(FILES_DIR, "otis_merged.nc")
+    zarrfile = os.path.join(FILES_DIR, "otis_merged.zarr")
+    dset1 = read_otis_merged(ncfile)
+    dset2 = read_otis_merged(zarrfile, file_format="zarr")
+    assert dset1.equals(dset2)
+
+
+def test_read_otis_binary():
+    dset = read_otis_binary(os.path.join(FILES_DIR, "otis_binary/Model_rag"))
+
