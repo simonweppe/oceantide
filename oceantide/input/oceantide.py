be59566a19d28376d5220074ce6315b9bafe26a1
Rafael Guedes <r.guedes@oceanum.science>
2021-08-05 22:58:58 +1200

    Remove fsspec explicit call, support old datasets in read_oceantide

--- oceantide/input/oceantide.py
+++ oceantide/input/oceantide.py
@@ -1,17 +1,17 @@
 """Read Oceantide file format."""
+import warnings
 import xarray as xr
-from fsspec import get_mapper
 
 from oceantide.tide import Tide
 
 
-def read_oceantide(filename=None, zarr_kwargs={"consolidated": True}):
+def read_oceantide(filename=None, engine="zarr", backend_kwargs={"consolidated": True}):
     """Read Oceantide file format.
 
     Args:
-        filename (str): Name of `Model_*` metadata file specifying other files to read.
-            Only zarr is supported as netcdf does not deal well with complex array.
-        zarr_kwargs (dict): Keywargs to pass to xr.open_zarr.
+        filename (str): Name of Oceantide dataset to read.
+        engine (str): Engine to pass to xr.open_dataset.
+        backend_kwargs (dict): Keywargs to pass to xr.open_dataset.
 
     Returns:
         Formatted dataset with the tide accessor.
@@ -26,11 +26,21 @@ def read_oceantide(filename=None, zarr_kwargs={"consolidated": True}):
     * lat      (lat) float64 48.47 48.53 48.58 48.63 ... 60.87 60.92 60.97 61.03
     * lon      (lon) float64 -11.02 -10.97 -10.92 -10.87 ... 12.92 12.97 13.02
     Data variables:
-        depth    (lat, lon) float32 dask.array<chunksize=(126, 241), meta=np.ndarray>
-        et       (con, lat, lon) complex128 dask.array<chunksize=(3, 126, 241), meta=np.ndarray>
-        ut       (con, lat, lon) complex128 dask.array<chunksize=(3, 126, 241), meta=np.ndarray>
-        vt       (con, lat, lon) complex128 dask.array<chunksize=(3, 126, 241), meta=np.ndarray>
+        dep     (lat, lon) float32 dask.array<chunksize=(126, 241), meta=np.ndarray>
+        h       (con, lat, lon) complex128 dask.array<chunksize=(3, 126, 241), meta=np.ndarray>
+        u       (con, lat, lon) complex128 dask.array<chunksize=(3, 126, 241), meta=np.ndarray>
+        v       (con, lat, lon) complex128 dask.array<chunksize=(3, 126, 241), meta=np.ndarray>
 
     """
-    fsmap = get_mapper(filename)
-    return xr.open_zarr(fsmap, consolidated=True)
+    dset = xr.open_dataset(filename, engine="zarr", backend_kwargs=backend_kwargs)
+
+    # For backward compatibility
+    if "et" in dset.data_vars and "h" not in dset.data_vars:
+        warnings.warn(
+            "Oceantide naming convention has been changed, only datasets with "
+            "variables ['dep','h','u','v'] will be supported in the future.",
+            category=FutureWarning,
+        )
+    dset = dset.rename({"et": "h", "ut": "u", "vt": "v", "depth": "dep"})
+
+    return dset
