729f0650e57c5a99ced126fd3a0979870d77abb1
Rafael Guedes <r.guedes@oceanum.science>
2021-08-15 22:17:09 +1200

    Starting docs

--- docs/conf.py
+++ docs/conf.py
@@ -20,10 +20,11 @@
 #
 import os
 import sys
-
-sys.path.insert(0, os.path.abspath(".."))
-
 import oceantide
+import sphinx_rtd_theme
+
+
+sys.path.insert(0, os.path.abspath("../"))
 
 # -- General configuration ---------------------------------------------
 
@@ -33,7 +34,26 @@ import oceantide
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.githubpages",
+    "sphinx_gallery.gen_gallery",
+    "IPython.sphinxext.ipython_console_highlighting",
+    "IPython.sphinxext.ipython_directive",
+    "matplotlib.sphinxext.plot_directive",
+]
+
+sphinx_gallery_conf = {
+    "examples_dirs": "gallery",
+    "gallery_dirs": "auto_gallery",
+}
+
+autosummary_generate = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -85,7 +105,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -98,6 +118,8 @@ html_theme = "alabaster"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+# The name of an image file to use as favicon
+# html_favicon = "_static/wavespectra_logo.png"
 
 # -- Options for HTMLHelp output ---------------------------------------
 
@@ -134,6 +156,8 @@ latex_documents = [
         "manual",
     ),
 ]
+
+# latex_logo = "_static/wavespectra_logo.png"
 
 
 # -- Options for manual page output ------------------------------------
