74a9bc135ea01249246aad9b9e717ebd51192132
Rafael Guedes <r.guedes@oceanum.science>
2021-08-17 16:02:52 +1200

    Change signature of read_oceantide

--- oceantide/input/oceantide.py
+++ oceantide/input/oceantide.py
@@ -5,39 +5,20 @@ import xarray as xr
 from oceantide.tide import Tide
 
 
-def read_oceantide(
-    filename=None, engine="zarr", backend_kwargs={"consolidated": True}, **kwargs
-):
+def read_oceantide(filename=None, engine="zarr", chunks={}, **kwargs):
     """Read Oceantide file format.
 
     Args:
         - filename (str): Name of Oceantide dataset to read.
         - engine (str): Engine to pass to xr.open_dataset.
-        - backend_kwargs (dict): Keywargs to pass to xr.open_dataset.
+        - chunks (dict): Chunk sizes along each dimensions to pass to xr.open_dataset.
         - kwargs: Extra kwargs to pass to xr.open_dataset.
 
     Returns:
         - Formatted dataset with the tide accessor.
 
-    Tide constituents datasets read with any reader function in oceantide returns
-        datasets in the oceantide format. The dataset should look like:
-
-    <xarray.Dataset>
-    Dimensions:  (con: 9, lat: 251, lon: 481)
-    Coordinates:
-    * con      (con) <U2 'M2' 'S2' 'N2' 'K2' 'K1' 'O1' 'P1' 'Q1' 'M4'
-    * lat      (lat) float64 48.47 48.53 48.58 48.63 ... 60.87 60.92 60.97 61.03
-    * lon      (lon) float64 -11.02 -10.97 -10.92 -10.87 ... 12.92 12.97 13.02
-    Data variables:
-        dep     (lat, lon) float32 dask.array<chunksize=(126, 241), meta=np.ndarray>
-        h       (con, lat, lon) complex128 dask.array<chunksize=(3, 126, 241), meta=np.ndarray>
-        u       (con, lat, lon) complex128 dask.array<chunksize=(3, 126, 241), meta=np.ndarray>
-        v       (con, lat, lon) complex128 dask.array<chunksize=(3, 126, 241), meta=np.ndarray>
-
     """
-    dset = xr.open_dataset(
-        filename, engine=engine, backend_kwargs=backend_kwargs, **kwargs
-    )
+    dset = xr.open_dataset(filename, engine=engine, chunks=chunks, **kwargs)
 
     # For backward compatibility
     if "et" in dset.data_vars and "h" not in dset.data_vars:
