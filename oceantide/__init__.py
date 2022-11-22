352ebf24637278471eb9646171ef67a482921068
Rafael Guedes <r.guedes@oceanum.science>
2022-11-22 15:56:49 +1300

    Bump version

--- oceantide/__init__.py
+++ oceantide/__init__.py
@@ -1,6 +1,6 @@
 """Top-level package for oceantide."""
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 __author__ = "Oceanum Developers"
 __contact__ = "developers@oceanum.science"
 __url__ = "http://github.com/wavespectra/oceantide"
