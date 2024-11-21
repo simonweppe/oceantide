8a873f30b3e3fb8440a59103e19aad0f2187d031
simonweppe <simon.weppe@gmail.com>
2024-11-21 11:34:25 +0100

    Update oceantide.py
    Modify read_oceantide to support the reading of regular tidal grids extracted from Datamesh, that have variables as ['u_re', 'u_im', etc..] 
    This is done by allowing user to specify suffix strings for the real and imaginary variables
    
    e.g. 
    import oceantide
    tide_cons_oceanum = oceantide.input.oceantide.read_oceantide('oceanum_global_multiscale_tidegrid_subset.nc', suffix  = ['_re','_im'])

--- oceantide/input/oceantide.py
+++ oceantide/input/oceantide.py
@@ -6,7 +6,7 @@ from oceantide.core.utils import set_attributes
 
 
 def read_oceantide(
-    filename: str = None, engine: str = None, chunks: dict = {}, **kwargs
+    filename: str = None, engine: str = None, chunks: dict = {}, suffix  = ['_real','_imag'],**kwargs
 ) -> xr.Dataset:
     """Read oceantide file format.
 
@@ -23,6 +23,8 @@ def read_oceantide(
     chunks (dict)
         Chunk sizes along each dimensions to pass to xr.open_dataset, by default use
         chunking specified on disk.
+    suffix (list of string)
+        Suffix used for the real and imaginary variables. Default : ['_real','_imag']
     kwargs
         Extra kwargs to pass to xr.open_dataset.
 
@@ -35,7 +37,8 @@ def read_oceantide(
     dset = xr.open_dataset(filename, engine=engine, chunks=chunks, **kwargs)
     dsout = dset[["dep"]]
     for v in ["h", "u", "v"]:
-        dsout[v] = dset[f"{v}_real"] + 1j * dset[f"{v}_imag"]
+        dsout[v] = dset[f"{v}{suffix[0]}"] + 1j * dset[f"{v}{suffix[1]}"]
+        # dsout[v] = dset[f"{v}_real"] + 1j * dset[f"{v}_imag"]
     set_attributes(dsout, "dataset")
     dsout["con"] = dsout.con.astype("U4")
     return dsout
